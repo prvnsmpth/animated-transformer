5fb4d5fac5fd3a21fe960eead764fdd084acefa9
Praveen Sampath <praveensampath2508@gmail.com>
2023-06-30 20:58:13 +0530

    Update meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
   import '../app.css'
+
+  import { MetaTags } from 'svelte-meta-tags'
 </script>
 
 <svelte:head>
   <title>The Animated Transformer</title>
   <script defer data-domain="prvnsmpth.github.io" src="https://plausible.io/js/script.js"></script>
 </svelte:head>
+
+<MetaTags
+  title="The Animated Transformer"
+  description="An intuitive explanation of the Transformer model architecture."
+  openGraph={{
+    url: 'https://prvnsmpth.github.io/animated-transformer',
+    title: 'The Animated Transformer',
+    description: 'An intuitive explanation of the Transformer model architecture.'
+  }}
+/>
 
 <slot />
